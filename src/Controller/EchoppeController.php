9e55f43c3598e76ed3a3eb5fbe412bf9e1a16175
Saint-ouen Jimmy <jimmy.saintouen@gmail.com>
2021-10-19 09:48:31 +0200

    lien BD + affichage échoppe et détail produit

--- src/Controller/EchoppeController.php
+++ src/Controller/EchoppeController.php
@@ -5,13 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProduitRepository;
 
 #[Route('/echoppe')]
 class EchoppeController extends AbstractController
 {
 	#[Route('/', name:'echoppe_index', methods:['GET'])]
-	public function index(): Response
+	public function index(ProduitRepository $produitRepository): Response
 	{
-		return $this->render('echoppe/index.html.twig');
+		return $this->render('echoppe/index.html.twig', ['produits' => $produitRepository->findAll(
+
+		)]);
 	}
 }
