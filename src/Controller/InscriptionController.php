3bd3d08f1016b3222cc7dd6e4212e0c89860dfa4
Asus <gomezfelix.fac@hotmail.com>
2021-10-29 12:17:49 +0200

    correction UserType.php et formulaire operationel

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -3,18 +3,37 @@
 namespace App\Controller;
 
 use App\Form\InscriptionType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
 
 class InscriptionController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager){
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/inscription', name: 'inscription')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $user = new User();
         $form = $this->createForm(InscriptionType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user= $form->getData();
+
+            $doctrine = $this->getDoctrine()->getManager();
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+        }
 
         return $this->render('inscription/index.html.twig',[
             'form'=> $form->createView()
--- src/Form/InscriptionType.php
+++ src/Form/InscriptionType.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +17,34 @@ class InscriptionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('password')
-            ->add('prenom')
-            ->add('nom')
-            ->add('telephone')
-        ;
+            ->add('nom', TextType::class, [
+                'label'=>'Nom :'
+
+            ])
+            ->add('prenom', TextType::class, [
+                'label'=>'Prenom :'
+
+            ])
+            ->add('password',RepeatedType::class,[
+                'type'=> PasswordType::class,
+                'invalid_message'=> 'la confirmation du mot de passe ne correspond pas',
+                'required'=>true,
+                'label'=>'Mot de passe :',
+                'first_options'=>[ 'Mot de passe :']
+            ])
+            ->add('email',RepeatedType::class,[
+                'type'=> EmailType::class,
+                'invalid_message'=> 'les emails ne correspondes pas',
+                'required'=>true,
+                'label'=> 'Adresse e-mail :',
+                'first_options'=>[ 'label'=>'Adresse e-mail :']
+            ])
+            ->add('telephone',TextType::class,[
+                'label' =>'Telephone :'
+            ])
+            ->add('Envoyer', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-success'],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
