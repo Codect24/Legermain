5e866dadbc35cd4cf1fd2c5f7bf06f6f7795c198
Saint-ouen Jimmy <jimmy.saintouen@gmail.com>
2021-11-09 08:21:57 +0100

    Image + filtre de catégorie

---
+++ src/Entity/Order.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=OrderRepository::class)
+ * @ORM\Table(name="`order`")
+ */
+class Order
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToMany(targetEntity=OrderItem::class, mappedBy="orderRef", cascade={"persist", "remove"}, orphanRemoval=true)
+     */
+    private $items;
+
+	const STATUT_CART = 'cart';
+
+	/**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $status = self::STATUT_CART;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateCreation;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateUpdate;
+
+    public function __construct()
+    {
+        $this->items = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Collection|OrderItem[]
+     */
+    public function getItems(): Collection
+    {
+        return $this->items;
+    }
+
+    public function addItem(OrderItem $item): self
+    {
+		// if item already exist, sum the quantities
+		foreach ($this->getItems() as $existingItem) {
+			if ($existingItem->equals($item))
+			{
+				$existingItem->setQuantity($existingItem->getQuantity() + $item->getQuantity());
+				return $this;
+			}
+		}
+
+		// else create a new one
+		$this->items[] = $item;
+		$item->setOrderRef($this);
+
+        return $this;
+    }
+
+    public function removeItem(OrderItem $item): self
+    {
+        if ($this->items->removeElement($item)) {
+            // set the owning side to null (unless already changed)
+            if ($item->getOrderRef() === $this) {
+                $item->setOrderRef(null);
+            }
+        }
+
+        return $this;
+    }
+
+	/**
+	 * Remove all items of the order
+	 *
+	 * @return $this
+	 */
+	public  function removeItems(): self
+	{
+		foreach ($this->getItems() as $item)
+		{
+			$this->removeItem($item);
+		}
+		return $this;
+	}
+
+	public function getTotal(): float
+	{
+		$total = 0;
+		foreach ($this->getItems() as $item)
+		{
+			$total += $item->getTotal();
+		}
+		return $total;
+	}
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->dateCreation;
+    }
+
+    public function setDateCreation(\DateTimeInterface $dateCreation): self
+    {
+        $this->dateCreation = $dateCreation;
+
+        return $this;
+    }
+
+    public function getDateUpdate(): ?\DateTimeInterface
+    {
+        return $this->dateUpdate;
+    }
+
+    public function setDateUpdate(\DateTimeInterface $dateUpdate): self
+    {
+        $this->dateUpdate = $dateUpdate;
+
+        return $this;
+    }
+}
---
+++ src/Factory/OrderFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Order;
+use App\Entity\OrderItem;
+use App\Entity\Produit;
+
+class OrderFactory
+{
+	public function create(): Order
+	{
+		$order = new Order();
+		$order
+			->setStatut(Order::STATUT_CART)
+			->setDateCreation(new \DateTime())
+			->setDateUpdate(new \DateTime());
+		return $order;
+	}
+
+	public function createItem(Produit $produit): OrderItem
+	{
+		$item = new OrderItem();
+		$item
+			->setProduct($produit)
+			->setQuantity(1);
+		return $item;
+	}
+}
