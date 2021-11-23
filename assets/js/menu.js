3e5e6b0b6f88c18c982b15cf642649295ae04798
Er4goon <robenson.dragoon2000@hotmail.fr>
2021-11-23 10:58:07 +0100

    Setting up de main hover menu

---
+++ assets/js/menu.js
@@ -0,0 +1,10 @@
+console.log("test")
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
