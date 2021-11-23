5d515ba637b711fe39487ef1a59f62a9fada3a2c
Thomas <thomas.timar@hotmail.fr>
2021-11-23 09:49:02 +0100

    Branche contact - Design

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,16 +2,44 @@
 
 namespace App\Controller;
 
+use App\Entity\ContactInfos;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\Bridge\Google\Smtp\GmailTransport;
+use Symfony\Component\Mailer\Mailer;
+use App\Form\ContactInfosType;
 
 #[Route('/contact')]
 class ContactController extends AbstractController
 {
-	#[Route('/',name: 'contact_index', methods: ['GET'])]
-	public function index(): Response
+	#[Route('/',name: 'contact_index', methods: ['POST','GET'])]
+	public function index(Request $request, MailerInterface $mailer): Response
 	{
-		return $this->render('contact/index.html.twig');
+		$ContactInfos = new ContactInfos();
+		$form = $this->createForm(ContactInfosType::class);
+
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			$ContactInfos = $form->getData();
+			$message = (new Email())
+				->from($ContactInfos->getMail())
+				->to('thomas.timar@hotmail.fr')
+				->subject($ContactInfos->getTitle())
+				->text($ContactInfos->getMessage());
+
+			$mailer->send($message);
+			return $this->redirectToRoute('contact_index');
+
+		}
+	return $this->render('contact/index.html.twig', [
+		'form' => $form->createView(),
+		]);
 	}
+
 }
