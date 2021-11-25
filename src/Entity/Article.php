0fb38bb4a05823449be0fadaf4c072da8909ce3b
Saint-ouen Jimmy <jimmy.saintouen@gmail.com>
2021-11-26 00:05:55 +0100

    Article : fonctionnel Commentaires : fonctionnel + lien article et user

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\HttpFoundation\File\File;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=ArticleRepository::class)
@@ -15,169 +16,204 @@ use Vich\UploaderBundle\Mapping\Annotation as Vich;
  */
 class Article
 {
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+	/**
+	 * @ORM\Id
+	 * @ORM\GeneratedValue
+	 * @ORM\Column(type="integer")
+	 */
+	private $id;
+
+	/**
+	 * @ORM\Column(type="string", length=50)
+	 */
+	private $title;
+
+	/**
+	 * @ORM\Column(type="string", length=1000)
+	 */
+	private $content;
+
+	/**
+	 * @ORM\Column(type="datetime")
+	 * @var \DateTime
+	 */
+	private $publicationDate;
+
+	/**
+	 * @ORM\ManyToOne(targetEntity=User::class, inversedBy="relation")
+	 * @ORM\JoinColumn(nullable=false)
+	 */
+	private $user;
+
+	/**
+	 * @ORM\OneToMany(targetEntity=Comments::class, mappedBy="articleId")
+	 */
+	private $CommentsRelation;
+
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 * @var string
+	 */
+	private $image;
+
+	/**
+	 * @Vich\UploadableField(mapping="article_images", fileNameProperty="image")
+	 * @Assert\File(maxSize="2147483648")
+	 * @var File
+	 */
+	private $imageFile;
+
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $slug;
 
     /**
-     * @ORM\Column(type="string", length=50)
+     * @ORM\Column(type="boolean")
      */
-    private $title;
-
-    /**
-     * @ORM\Column(type="string", length=1000)
-     */
-    private $content;
-
-    /**
-     * @ORM\Column(type="datetime")
-     * @var \DateTime
-     */
-    private $publicationDate;
-
-    /**
-     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="relation")
-     * @ORM\JoinColumn(nullable=false)
-     */
-    private $user;
-
-    /**
-     * @ORM\OneToMany(targetEntity=Comments::class, mappedBy="articleId")
-     */
-    private $CommentsRelation;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     * @var string
-     */
-    private $image;
-
-    /**
-     * @Vich\UploadableField(mapping="article_images", fileNameProperty="image")
-     * @var File
-     */
-    private $imageFile;
+    private $highlighted;
 
 
-    public function setImageFile(File $image = null)
+	public function setImageFile(File $image = null)
+         	{
+         		$this->imageFile = $image;
+         	}
+
+	public function getImageFile()
+         	{
+         		return $this->imageFile;
+         	}
+
+	public function setImage($image)
+         	{
+         		$this->image = $image;
+         	}
+
+	public function getImage()
+         	{
+         		return $this->image;
+         	}
+
+	public function __construct()
+         	{
+         		$this->CommentsRelation = new ArrayCollection();
+         	}
+
+	public function getId(): ?int
+         	{
+         		return $this->id;
+         	}
+
+	public function getUser(): ?User
+         	{
+         		return $this->user;
+         	}
+
+	public function setUser(User $user): self
+         	{
+         		$this->user = $user;
+         
+         		return $this;
+         	}
+
+	public function getTitle(): ?string
+         	{
+         		return $this->title;
+         	}
+
+	public function setTitle(string $title): self
+         	{
+         		$this->title = $title;
+         
+         		return $this;
+         	}
+
+	public function getContent(): ?string
+         	{
+         		return $this->content;
+         	}
+
+	public function setContent(string $content): self
+         	{
+         		$this->content = $content;
+         
+         		return $this;
+         	}
+
+	public function getImageList(): ?string
+         	{
+         		return $this->imageList;
+         	}
+
+	public function setImageList(?string $imageList): self
+         	{
+         		$this->imageList = $imageList;
+         
+         		return $this;
+         	}
+
+	public function getPublicationDate(): ?\DateTime
+         	{
+         		return $this->publicationDate;
+         	}
+
+	public function setPublicationDate(\DateTime $publicationDate): self
+         	{
+         		$this->publicationDate = $publicationDate;
+         
+         		return $this;
+         	}
+
+	/**
+	 * @return Collection|Comments[]
+	 */
+	public function getCommentsRelation(): Collection
+         	{
+         		return $this->CommentsRelation;
+         	}
+
+	public function addCommentsRelation(Comments $commentsRelation): self
+         	{
+         		if (!$this->CommentsRelation->contains($commentsRelation)) {
+         			$this->CommentsRelation[] = $commentsRelation;
+         			$commentsRelation->setArticle($this);
+         		}
+         
+         		return $this;
+         	}
+
+	public function removeCommentsRelation(Comments $commentsRelation): self
+         	{
+         		if ($this->CommentsRelation->removeElement($commentsRelation)) {
+         			// set the owning side to null (unless already changed)
+         			if ($commentsRelation->getArticle() === $this) {
+         				$commentsRelation->setArticle(null);
+         			}
+         		}
+         
+         		return $this;
+         	}
+
+	public function getSlug(): ?string
+         	{
+         		return $this->slug;
+         	}
+
+	public function setSlug(string $slug): self
+         	{
+         		$this->slug = $slug;
+         
+         		return $this;
+         	}
+
+    public function getHighlighted(): ?bool
     {
-        $this->imageFile = $image;
+        return $this->highlighted;
     }
 
-    public function getImageFile()
+    public function setHighlighted(bool $highlighted): self
     {
-        return $this->imageFile;
-    }
-
-    public function setImage($image)
-    {
-        $this->image = $image;
-    }
-
-    public function getImage()
-    {
-        return $this->image;
-    }
-
-    public function __construct()
-    {
-        $this->CommentsRelation = new ArrayCollection();
-    }
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getUser(): ?User
-    {
-        return $this->user;
-    }
-
-    public function setUser(User $user): self
-    {
-        $this->user = $user;
-
-        return $this;
-    }
-
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
-
-    public function setTitle(string $title): self
-    {
-        $this->title = $title;
-
-        return $this;
-    }
-
-    public function getContent(): ?string
-    {
-        return $this->content;
-    }
-
-    public function setContent(string $content): self
-    {
-        $this->content = $content;
-
-        return $this;
-    }
-
-    public function getImageList(): ?string
-    {
-        return $this->imageList;
-    }
-
-    public function setImageList(?string $imageList): self
-    {
-        $this->imageList = $imageList;
-
-        return $this;
-    }
-
-    public function getPublicationDate(): ?\DateTime
-    {
-        return $this->publicationDate;
-    }
-
-    public function setPublicationDate(\DateTime $publicationDate): self
-    {
-        $this->publicationDate = $publicationDate;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Comments[]
-     */
-    public function getCommentsRelation(): Collection
-    {
-        return $this->CommentsRelation;
-    }
-
-    public function addCommentsRelation(Comments $commentsRelation): self
-    {
-        if (!$this->CommentsRelation->contains($commentsRelation)) {
-            $this->CommentsRelation[] = $commentsRelation;
-            $commentsRelation->setArticleId($this);
-        }
-
-        return $this;
-    }
-
-    public function removeCommentsRelation(Comments $commentsRelation): self
-    {
-        if ($this->CommentsRelation->removeElement($commentsRelation)) {
-            // set the owning side to null (unless already changed)
-            if ($commentsRelation->getArticleId() === $this) {
-                $commentsRelation->setArticleId(null);
-            }
-        }
+        $this->highlighted = $highlighted;
 
         return $this;
     }
